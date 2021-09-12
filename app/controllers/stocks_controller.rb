5f85b1c9839007817469055f5568640e3f2a6d85
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-12 13:24:58 -0300

    use ajax for form submission

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,16 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render '/users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        render '/users/my_portfolio'
+      else
+        flash[:alert] = "Não foi encontrado nenhuma ação com o codigo: #{params[:stock]}"
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = 'Por favor entre com o codigo para pesquisar'
+      redirect_to my_portfolio_path
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,11 +1,19 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(
+    client = client_connect
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name,
+          last_price: client.price(ticker_symbol))
+    rescue StandardError
+      nil
+    end
+  end
+
+  def self.client_connect
+    IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.publishable_token,
       secret_token: Rails.application.credentials.secret_token,
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name,
-        last_price: client.price(ticker_symbol))
   end
 end
