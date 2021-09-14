05d5cb481a8ad4a026ec6cd9423931b116a69a7c
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-14 14:37:05 -0300

    add JS response to form - search friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,25 @@ class UsersController < ApplicationController
   def friends
     @friends = current_user.friends
   end
+
+  def search
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        flash.now[:alert] = "Não foi encontrado nenhum usuario com este nome ou email: #{params[:friend]}"
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else
+      flash.now[:alert] = 'Por favor entre com nome ou email para pesquisar'
+      respond_to do |format|
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
+  end
 end
